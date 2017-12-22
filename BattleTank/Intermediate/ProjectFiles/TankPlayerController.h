5b4405381f29b0ae1576cb9c53268124225daa42
Lyubomir Gardev <l.gardev@hellohungry.com>
2017-12-22 15:54:08 +0200

    Added TankPlayerController.

---
+++ BattleTank/Intermediate/ProjectFiles/TankPlayerController.h
@@ -0,0 +1,20 @@
+//
+//  TankPlayerController.h
+//  BattleTank
+//
+//  Created by Lyubomir Gardev on 22.12.17.
+//  Copyright © 2017 Epic Games, Inc. All rights reserved.
+//
+
+#ifndef TankPlayerController_h
+#define TankPlayerController_h
+
+#include "Runtime/Engine/Classes/GameFramework/PlayerController.h"
+
+UCLASS()
+class BATTLETANK_API TankPlayerController : public APlayerController
+{
+    
+}
+
+#endif /* TankPlayerController_h */
