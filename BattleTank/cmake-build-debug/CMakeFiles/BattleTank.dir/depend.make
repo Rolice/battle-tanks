0a2e233c8ebcbe44d164c7c37b5ff54c251cc311
Lyubomir Gardev <l.gardev@hellohungry.com>
2017-12-25 09:57:38 +0200

    CreateSubObject...

--- BattleTank/cmake-build-debug/CMakeFiles/BattleTank.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for BattleTank.
-# This may be replaced when dependencies are built.
--- BattleTank/cmake-build-debug/CMakeFiles/BattleTankEditor-Development.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/BattleTankEditor-Development"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/BattleTankEditor-Development.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
